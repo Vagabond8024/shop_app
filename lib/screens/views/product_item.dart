b2da8a8f2d13781fd86c3b78fa5fabeae23b5e01
Bohdan <lan8024@gmail.com>
2022-08-18 19:33:29 +0300

    added product overview screen

---
+++ lib/screens/views/product_item.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  final String id;
+  final String title;
+  final String imageUrl;
+
+  const ProductItem(
+      {Key? key, required this.id, required this.title, required this.imageUrl})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(11),
+      child: GridTile(
+        footer: GridTileBar(
+            leading: IconButton(
+              color: Theme.of(context).colorScheme.secondary,
+              icon: const Icon(Icons.favorite),
+              onPressed: () {},
+            ),
+            trailing: IconButton(
+                color: Theme.of(context).colorScheme.secondary,
+                icon: const Icon(
+                  Icons.shopping_bag,
+                ),
+                onPressed: (() {})),
+            backgroundColor: Colors.black87,
+            title: Text(
+              title,
+              textAlign: TextAlign.center,
+            )),
+        child: Image.network(
+          imageUrl,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+}
