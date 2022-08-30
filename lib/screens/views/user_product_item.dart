18bee7db482fb6db31c2959039381b7e10c2a208
Bohdan <lan8024@gmail.com>
2022-08-30 22:40:45 +0300

    added delelte product functionality

--- lib/screens/views/user_product_item.dart
+++ lib/screens/views/user_product_item.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop_app/providers/products_provider.dart';
 
 class UserProductItem extends StatelessWidget {
   final String id;
@@ -27,7 +29,10 @@ class UserProductItem extends StatelessWidget {
                 icon: const Icon(Icons.edit),
                 color: Theme.of(context).colorScheme.primary),
             IconButton(
-              onPressed: () {},
+              onPressed: () {
+                Provider.of<ProductsProvider>(context, listen: false)
+                    .deleteProduct(id);
+              },
               icon: const Icon(Icons.delete),
               color: Theme.of(context).errorColor,
             ),
