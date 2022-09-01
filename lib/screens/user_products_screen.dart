02564687889c988bf761654af40c390f9097a2c7
Bohdan <lan8024@gmail.com>
2022-09-01 23:24:14 +0300

    added pull to refresh

--- lib/screens/user_products_screen.dart
+++ lib/screens/user_products_screen.dart
@@ -7,6 +7,11 @@ import 'package:shop_app/screens/views/user_product_item.dart';
 class UserProductsScreen extends StatelessWidget {
   const UserProductsScreen({Key? key}) : super(key: key);
 
+  Future<void> _refreshProduct(BuildContext context) async {
+    await Provider.of<ProductsProvider>(context, listen: false)
+        .fetchAndSetProduct();
+  }
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<ProductsProvider>(context);
@@ -22,22 +27,27 @@ class UserProductsScreen extends StatelessWidget {
           ],
         ),
         drawer: const AppDrawer(),
-        body: Padding(
-          padding: const EdgeInsets.all(8),
-          child: ListView.builder(
-            itemCount: productsData.items.length,
-            itemBuilder: (_, index) {
-              return Column(
-                children: [
-                  UserProductItem(
-                    id: productsData.items[index].id,
-                    title: productsData.items[index].title,
-                    imageUrl: productsData.items[index].imageUrl,
-                  ),
-                  const Divider()
-                ],
-              );
-            },
+        body: RefreshIndicator(
+          onRefresh: () {
+            return _refreshProduct(context);
+          },
+          child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: ListView.builder(
+              itemCount: productsData.items.length,
+              itemBuilder: (_, index) {
+                return Column(
+                  children: [
+                    UserProductItem(
+                      id: productsData.items[index].id,
+                      title: productsData.items[index].title,
+                      imageUrl: productsData.items[index].imageUrl,
+                    ),
+                    const Divider()
+                  ],
+                );
+              },
+            ),
           ),
         ));
   }
