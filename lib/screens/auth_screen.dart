b0f1f2e5a5dbf97f5d031bd5d0b6adae3ff96452
Bohdan <lan8024@gmail.com>
2022-09-11 22:17:02 +0300

    add animations on auth screen

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -93,7 +93,8 @@ class AuthCard extends StatefulWidget {
   State<AuthCard> createState() => _AuthCardState();
 }
 
-class _AuthCardState extends State<AuthCard> {
+class _AuthCardState extends State<AuthCard>
+    with SingleTickerProviderStateMixin {
   final GlobalKey<FormState> _formKey = GlobalKey();
   AuthMode _authMode = AuthMode.login;
   final Map<String, String?> _authData = {
@@ -102,6 +103,29 @@ class _AuthCardState extends State<AuthCard> {
   };
   var _isLoading = false;
   final _passwordController = TextEditingController();
+  AnimationController? _controller;
+  Animation<Size>? _heightAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 500));
+    _heightAnimation = Tween<Size>(
+            begin: Size(double.infinity, 260), end: Size(double.infinity, 320))
+        .animate(CurvedAnimation(
+            parent: _controller as Animation<double>,
+            curve: Curves.fastOutSlowIn));
+    _heightAnimation!.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller!.dispose();
+  }
 
   void _showErrorDialog(String message) {
     showDialog(
@@ -166,10 +190,12 @@ class _AuthCardState extends State<AuthCard> {
       setState(() {
         _authMode = AuthMode.signup;
       });
+      _controller!.forward();
     } else {
       setState(() {
         _authMode = AuthMode.login;
       });
+      _controller!.reverse();
     }
   }
 
@@ -182,7 +208,8 @@ class _AuthCardState extends State<AuthCard> {
       ),
       elevation: 8.0,
       child: Container(
-        height: _authMode == AuthMode.signup ? 320 : 260,
+        // height: _authMode == AuthMode.signup ? 320 : 260,
+        height: _heightAnimation!.value.height,
         constraints:
             BoxConstraints(minHeight: _authMode == AuthMode.signup ? 320 : 260),
         width: deviceSize.width * 0.75,
